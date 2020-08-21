d93f5156b21acf5fb1d74ccba5779821226886b7
reic <reic.wang@gmail.com>
2020-08-22 00:14:18 +0800

    version 1

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # 85%原則是最佳學習策略 (85% Rules for Optimal Learning)
+
 這是一個 Python 工具，用來判斷英文文件是否符合自己的程度。本文參考的是 Nature Communication 文獻的一篇實作，文章相關的介紹可以參考 [最有效的學習策略：85%原則](https://twreic.blogspot.com/2020/07/85.html)
 
 This is a Python Tool used to know if an English article is suitable for you. This is based the result of the aritcle [The Eighty Five Percent Rule for optimal learning](https://doi.org/10.1038/s41467-019-12552-4) published in Nature Commincation.
+
+## 如何標記 How to mark the unknown word
+
+本程式依據 Microsft Word 檔案不同格式做為判斷是否為不認識的單字，並會計算不認識的單字占總字數的比例，計算出意外率。當 1 - 意外率 < 85% 時，代表這一篇文章從文字的熟悉度來看，對你來說太難了。
+
+在 Word 中，如何標記不熟悉的英文單字呢？ 本程式採用 雙重下底線，只需要選擇英文字，再透過 Ctrl-Shift-D 熱鍵，即可完成標記。
+
+## 如何設置 How to configure
+
+本程式為 Python 程式，需要額外安裝 nltk, python-docx 兩個模組，
+
+- pip install nltk
+- pip install python-docx
+
+預設的 docx 檔案為 test.docx，不熟的英文單字會記錄在 wordlearn.txt 內。你也可以透過程式最下面的 options 的值，即可
+
+```python
+if __name__ == "__main__":
+    #  filename 為 docx 的檔案名稱，預設為 test.docx
+    #  output_filename 為輸出的檔案名稱，預設為 wordlearn.txt
+    options = {'filename': "test.docx", 'output_filename': 'wordlearn.txt'}
+```
---
+++ doc.py
@@ -0,0 +1,86 @@
+# pip install python-docx
+# pip install nltk
+import docx
+import re
+from nltk.stem import PorterStemmer
+
+
+def get_words(data, pattern=r'[a-zA-Z]+'):
+    words = re.findall(pattern, data)
+    return words
+
+
+class Wordobj(object):
+    def __init__(self, word, counter=1):
+        self.counter = counter
+        self.word = word
+
+    def __str__(self):
+        return " %s \t %d " % (self.word, self.counter)
+
+
+def word_count(words):
+    stems = {}
+    ps = PorterStemmer()
+    for word in words:
+        word = word.lower()
+        index = ps.stem(word)
+        if index not in stems:
+            stems[index] = Wordobj(word)
+        else:
+            stems[index].counter += 1
+            len_word = len(word)
+            len_stem_word = len(stems[index].word)
+            if len_word < len_stem_word:
+                stems[index].word = word
+            if len_word == len_stem_word and word != stems[index].word:
+                stems[index].word = word
+    return stems
+
+
+def get_doc_words(filename):
+    # read docx
+    readfile = docx.Document("./"+filename)
+    wordlist, unknowlist = [], []
+    for para in readfile.paragraphs:
+        wordlist.extend(get_words(para.text.lower()))
+        for run in para.runs:
+            if run.underline and run.underline != True:
+                # unknowlist.append(run.text.lower())
+                unknowlist.extend(get_words(run.text.lower()))
+    return wordlist, unknowlist
+
+
+def unknownwords_to_file(filename, data):
+    text = ', '.join([item.word for item in data.values()])+"\n"
+    with open('./'+filename, "a", encoding='utf-8') as f:
+        f.write(text)
+
+
+def main(options):
+
+    # 從 word 檔讀取文字， 不認識的字  double underline (ctrl - shift - d) 的字
+    wordall, unknowns = get_doc_words(options['filename'])
+    # print(wordall)
+    stemlist = word_count(wordall)
+    unknown_stemlist = word_count(unknowns)
+    counter = 0
+    for item in unknown_stemlist:
+        counter += stemlist[item].counter
+        unknown_stemlist[item].word = stemlist[item].word
+        unknown_stemlist[item].counter = stemlist[item].counter
+    accident_rate = counter/len(wordall)*100
+    print("總共單字數 %2d" % len(stemlist))
+    print("不熟悉的單字 %2d" % len(unknown_stemlist))
+    print("意外率 %.2f %%" % accident_rate)
+    print(' 不認識的單字 '.center(100, '-'))
+    print({item.word: item.counter for item in unknown_stemlist.values()})
+    # 將新的單字寫入檔案
+    unknownwords_to_file(options['output_filename'], unknown_stemlist)
+
+
+if __name__ == "__main__":
+    #  filename 為 docx 的檔案名稱，預設為 test.docx
+    #  output_filename 為輸出的檔案名稱，預設為 wordlearn.txt
+    options = {'filename': "test.docx", 'output_filename': 'wordlearn.txt'}
+    main(options)
